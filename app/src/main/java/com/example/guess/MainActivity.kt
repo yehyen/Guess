10bb836b326dc7b962157c3e2abf5360c3d5bf7c
yeh <md32513@gmail.com>
2021-02-05 13:51:08 +0800

    set LinearLayout

--- app/src/main/java/com/example/guess/MainActivity.kt
+++ app/src/main/java/com/example/guess/MainActivity.kt
@@ -10,7 +10,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.linear_main)
 
     }
 }
