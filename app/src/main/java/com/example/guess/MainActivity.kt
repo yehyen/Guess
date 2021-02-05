3cd11028f2ba81206cf814a3e4fab7636f9fb103
yeh <md32513@gmail.com>
2021-02-05 14:58:25 +0800

    Activities between layout & MainActivity

--- app/src/main/java/com/example/guess/MainActivity.kt
+++ app/src/main/java/com/example/guess/MainActivity.kt
@@ -2,6 +2,9 @@ package com.example.guess
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -10,7 +13,18 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.linear_main)
+        setContentView(R.layout.activity_main)
 
+    }
+    // 與onClick事件互動，button>CommonAttributes>onClick連動，onClick名稱與fun名稱要相同
+    // 參數型態為View類別，即在Acivity上所顯示的控件
+    fun check(view: View){
+
+        //取得EditText輸入後得到的數字，id=number，使用屬性text取得內容後用toString()得到字串，最後再轉成toInt()
+        val n = number.text.toString().toInt()
+        println("number: $n")
+
+        // 訊息處理Log類別，用.d方便除錯記錄，參數1指來自class名稱，參數2指出現的內容
+        Log.d("MainActivity", "number:" + n)
     }
 }
