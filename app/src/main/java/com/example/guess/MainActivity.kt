5dbb9b7d110c6681c205ecc48662bf5fadd68aa5
yeh <md32513@gmail.com>
2021-02-18 16:17:32 +0800

    build RecyclerView(LinearLayout)

--- app/src/main/java/com/example/guess/MainActivity.kt
+++ app/src/main/java/com/example/guess/MainActivity.kt
@@ -3,59 +3,78 @@ package com.example.guess
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.row_function.view.*
 
 class MainActivity : AppCompatActivity() {
 
-    // 產生猜數字物件
-    val secretNumber = SecretNumber()
     // 得到MainActivity的字串，讓Log除錯用
     val TAG = MainActivity::class.java.simpleName
+
+    //顯示項目
+    val functions = listOf<String>(
+            "Camera",
+            "Invite friend",
+            "Parking",
+            "Download coupons",
+            "A",
+            "B",
+            "B",
+            "B",
+            "B",
+            "B",
+            "B",
+            "B",
+            "News",
+            "Maps")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Log.d(TAG, "secret:" + secretNumber.secret)
 
+        //RecyclerView：當user上下滑動元件離開畫面時，該元件會先被儲存等待之後被取出
+        //早期是用丟棄方式
+        //設定版面功能表：LinearLayout清單流水式，可以上下滑動
+        recycler.layoutManager = LinearLayoutManager(this)
+        // 是否固定大小
+        recycler.setHasFixedSize(true)
+        // adapter連接器，實現一個或多個系統不同部分之間的連接，獲得協同處理
+        recycler.adapter = FunctionAdapter()
     }
 
-    // 與onClick事件互動，button>CommonAttributes>onClick連動，onClick名稱與fun名稱要相同
-    // 參數型態為View類別，即在Acivity上所顯示的控件
-    fun check(view: View){
+    //配置畫面的holder，必須繼承RecyclerView.Adapter來做畫面功能
+    //內部類別，只能給MainActivity用
+    inner class FunctionAdapter(): RecyclerView.Adapter<FunctionHolder>(){
 
-        //取得EditText輸入後得到的數字，id=number，使用屬性text取得內容後用toString()得到字串，最後再轉成toInt()
-        val n = number.text.toString().toInt()
-        println("number: $n")
-
-        // 訊息處理Log類別，用.d方便除錯記錄，參數1指來自class名稱，參數2指出現的內容
-        Log.d(TAG, "number:" + n)
-
-        // 與使用者互動，輸入值來得到提示
-        val diff = secretNumber.validate(n)
-        var message = getString(R.string.yes_you_got_it)    // 字串抽取成資源共用，string.xml
-        if(diff < 0){
-            message = getString(R.string.bigger)
-        }else if(diff > 0){
-            message = getString(R.string.smaller)
+        //設定資料顯示畫面
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FunctionHolder {
+            val view = LayoutInflater.from(parent.context)
+                    .inflate(R.layout.row_function, parent, false)
+            val holder = FunctionHolder(view)
+            return holder
         }
 
-        //提示內容Toast類別：浮動元件
-        // makeText()參數1需放入Context類別，MainActivity有繼承到Context類別
-        // this代表MainActivity物件
-        // 參數2指給使用者看的內容
-        // 參數3指持續時間LENGTH_LONG=1
-//        Toast.makeText(this, message, Toast.LENGTH_LONG).show()
+        // 當有資料時，設定資料位置；position：資料在第幾列
+        override fun onBindViewHolder(holder: FunctionHolder, position: Int) {
+            holder.nameText.text = functions.get(position)
+        }
 
-        // 使用另一種提示元件：AlertDialog類別，彈出對話框
-        // Builder()可設計對話框內部的功能
-        AlertDialog.Builder(this)
-                .setTitle(getString(R.string.dialog_title))    // 設定標題
-                .setMessage(message)    // 設定訊息
-                .setPositiveButton(getString(R.string.ok), null)    // 設定正向按鈕，參數2指按下後的反應
-                .show()
+        // 裡面有多少筆資料
+        override fun getItemCount(): Int {
+            return functions.size
+        }
+    }
 
+    // 暫存功能
+    class FunctionHolder(view:View): RecyclerView.ViewHolder(view){
+        var nameText: TextView = view.name
     }
 }
